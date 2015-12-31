0cf955e695c261ad4db9e3b524e2ecd20221222d
bushwald <bradley.allen84@gmail.com>
2015-12-31 13:10:15 -0500

    lab solved

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,27 +1,26 @@
-# Implement your procedural solution here!
-$fibArr = Array.new
-$evenFib = 1
-$started = false
-$startNum = 0
-
-def even_fibonacci_sum(num)
-	$startNum = num if $started == false
-	$started = true
-	return num if num <= 1
-	exists = false
-	$fibArr.each{|x|
-		exists = true if x == $evenFib
-	}
-	if $evenFib % 2 == 0 && exists == false && $evenFib < $startNum
-		$fibArr << $evenFib
-	end
-	$evenFib = even_fibonacci_sum(num - 1) + even_fibonacci_sum(num - 2)
+def fib(idx)
+	return num if num < 2
+	fib(num-1) + fib(num-2)
 end
 
-
-even_fibonacci_sum(10)
-sum = 0
-$fibArr.each{|y|
-	sum += y
-}
-puts sum
+def even_fibonacci_sum(num)
+	sumArr = Array.new
+	i = 0
+	sum = 0
+	while(sum < n)
+		fib = fib(i)
+		exists = false
+		if fib % 2 == 0
+			sumArr.each{|x|
+				exists = true if fib == x
+			}
+			if exists == false && 
+				sumArr << fib
+				return sum if fib >= n
+				sum += fib
+			end
+		end
+		i += 1
+	end
+	return sum
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,34 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+	def initialize(num)
+		@num = num
+		@sum = 0
+		@idx = 0
+		@sumArr = Array.new
+		@exists = false
+	end
+
+	def sum
+		while(@sum < @num)
+			fib = fib(@idx)
+			if fib % 2 == 0
+				@sumArr.each{|x|
+					@exists = true if fib == x
+				}
+				if @exists == false && 
+					@sumArr << fib
+					return @sum if fib >= @num
+					@sum += fib
+				end
+			end
+			@idx += 1
+		end
+		return @sum
+	end
+
+	private
+	def fib(num)
+		return num if num < 2
+		fib(num-1) + fib(num-2)
+	end
+end
