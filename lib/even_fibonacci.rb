b2fd2b67a5a696394cf2ccb8e9c39472238233f5
bushwald <bradley.allen84@gmail.com>
2015-12-30 22:41:33 -0500

    recursive solution taking too long to run. More work to be done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,27 @@
 # Implement your procedural solution here!
+$fibArr = Array.new
+$evenFib = 1
+$started = false
+$startNum = 0
+
+def even_fibonacci_sum(num)
+	$startNum = num if $started == false
+	$started = true
+	return num if num <= 1
+	exists = false
+	$fibArr.each{|x|
+		exists = true if x == $evenFib
+	}
+	if $evenFib % 2 == 0 && exists == false && $evenFib < $startNum
+		$fibArr << $evenFib
+	end
+	$evenFib = even_fibonacci_sum(num - 1) + even_fibonacci_sum(num - 2)
+end
+
+
+even_fibonacci_sum(10)
+sum = 0
+$fibArr.each{|y|
+	sum += y
+}
+puts sum
